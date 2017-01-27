e5bdbf9a8d3ed79c343e1738cbf6ae0cee5e90ba
Kuei <ragnakuei@livemail.tw>
2017-01-28 00:19:12 +0800

    ok

---
+++ FindDupicateNumberTest/Tests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using FluentAssertions;
+using MyExtend;
+
+namespace FindDupicateNumberTest
+{
+	// 給定 小於 32 的不定長度正整數數量，取出重複的數字
+
+	[TestFixture]
+	public class Tests
+	{
+		[Test]
+		public void FindDupicateNumber_OneDupicateTwoNumber_Should_be_16()
+		{
+			int[] numbers = { 16, 16, 1 };
+			int[] expected = { 16 };
+
+			var actual = numbers.FindDuplicateNumber();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		[Test]
+		public void FindDupicateNumber_TwoDupicateTwoNumber_Should_be_1_16()
+		{
+			int[] numbers = { 1, 1, 31, 31, 16 };
+			int[] expected = { 1, 31 };
+
+			var actual = numbers.FindDuplicateNumber();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		[Test]
+		public void FindDupicateNumber_ThreeDupicateThreeNumber_Should_be_1_3()
+		{
+			int[] numbers = { 1, 1, 1, 3, 3, 3, 16, 2 };
+			int[] expected = { 1, 3 };
+
+			var actual = numbers.FindDuplicateNumber();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+	}
+}
---
+++ MyExtend/MyPage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace MyExtend
+{
+	public static class MyExtend
+	{
+		public static int[] FindDuplicateNumber(this int[] numbers)
+		{
+			Dictionary<int, int> duplicateNumbers = new Dictionary<int, int>();
+			foreach (var number in numbers)
+			{
+				if (duplicateNumbers.ContainsKey(number))
+				{ duplicateNumbers[number]++; }
+				else
+				{ duplicateNumbers.Add(number, 1); }
+			}
+			return duplicateNumbers.Where(i => i.Value >= 2).Select(i => i.Key).ToArray();
+		}
+	}
+}
